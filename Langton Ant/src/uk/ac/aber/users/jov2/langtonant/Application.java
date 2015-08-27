e94c8ace974de3379b66897d0191a662daf47dcc
Jose Vives <jota_sk@hotmail.com>
2015-08-27 12:38:24 +0200

    Testing if the library is correctly imported, and created the main class

--- Langton Ant/src/uk/ac/aber/users/jov2/langtonant/Application.java
+++ Langton Ant/src/uk/ac/aber/users/jov2/langtonant/Application.java
@@ -1,10 +1,37 @@
 package uk.ac.aber.users.jov2.langtonant;
 
-public class Application {
+import com.badlogic.gdx.ApplicationListener;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 
+public class Application implements ApplicationListener {
+
+	@Override
+	public void create() {}
+
+	@Override
+	public void dispose() {}
+
+	@Override
+	public void pause() {}
+
+	@Override
+	public void render() {}
+
+	@Override
+	public void resize(int arg0, int arg1) {}
+
+	@Override
+	public void resume() {}
+	
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.width = 800;
+		config.height = 600;
+		config.fullscreen = false;
+		
+		new LwjglApplication(new Application(), config);
 	}
 
 }
